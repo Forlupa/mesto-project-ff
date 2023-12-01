ef3cc0e307678a391f9ec980ebf06ee891151ba1
Forlupa <evengrase@yandex.ru>
2023-12-01 15:26:33 +0300

    написана логика добавления и удаления стартовых карточек

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,30 @@
-// @todo: Темплейт карточки
+const sectionCards = document.querySelector('.places__list')
 
-// @todo: DOM узлы
+function createCard(elem) {
+  const template = document.querySelector('#card-template')//нашли тимплейт
+  const cards = template.content.cloneNode(true)//клонировали его
+  const card = cards.querySelector('.card').cloneNode(true);//клонировали содержимое
+  const cardTitle = card.querySelector(".card__title");//нашли название
+  const cardImage = card.querySelector('.card__image');//нашли картинку
+  const cardDump = card.querySelector('.card__delete-button');//нашли кнопку с мусоркой
 
-// @todo: Функция создания карточки
+  cardTitle.textContent = elem.name;
+  cardImage.src = elem.link;
+  cardImage.alt = elem.name; //тут подставили значения для атрибутов картинки и для названия
 
-// @todo: Функция удаления карточки
+  cardDump.addEventListener('click', function () {
+    const cardItem = cardDump.closest('.card');
+    cardItem.remove();
+  }); 
 
-// @todo: Вывести карточки на страницу
+  return card; //Вернули готовую карточку
+}
+
+function addCard(elem) {
+  const card = createCard(elem); //Берем карточку
+  sectionCards.append(card); //и вставляем ее на место
+}
+
+initialCards.forEach((elem) => { 
+  addCard(elem); //Создаем карточку из каждого элемента массива
+});
